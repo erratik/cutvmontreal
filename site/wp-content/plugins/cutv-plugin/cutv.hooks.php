b8287f269bc846ee808f07d4c19ae7215c097c52
erratik <tay@erratik.ca>
2016-08-20 15:58:37 -0400

    added cutv-api plugin submodule, removed cutv-plugin - functionality moved to cutv-api plugin

--- site/wp-content/plugins/cutv-plugin/cutv.hooks.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-//	require_once( 'hooks/cutv_.hooks.assets.php');
-	require_once( 'hooks/cutv.hooks.core.php');
-//	require_once( 'hooks/cutv_.hooks.custom.php');
-	require_once( 'hooks/cutv.hooks.menus.php');
-//	require_once( 'hooks/cutv_.hooks.notices.php');
-//	require_once( 'hooks/cutv_.hooks.shortcodes.php');
-//	require_once( 'hooks/cutv_.hooks.rewrite.php');
